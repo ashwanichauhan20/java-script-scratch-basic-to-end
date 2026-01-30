0bf3c8f5bc89f434ee7479b8fd2b3c32846d72be
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-01-30 09:49:23 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accountEmail= "ashwanikumarchauhan014@gmail.com"
+var accountPassword="12345"
+accountCity ="jaipur"
+let accountState;
+// if value has not assigne so itself generate memory
+// accountId =2  //not allow
+accountEmail="ashwani9120@gmail.com"
+accountPassword="2313213"
+accountCity="Bengaluru"
+
+console.log(accoundId);
+/*prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
