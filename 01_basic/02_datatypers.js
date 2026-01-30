316d49212a132b4102d03347ba25ea369fab9d14
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-01-30 10:13:11 +0000

    data type done

---
+++ 01_basic/02_datatypers.js
@@ -0,0 +1,18 @@
+ "USE STRICT"; //treat all JScode as newer version
+//  alert(3+3) // we are using nodejs, not browser
+consloe.log(3+3); console.log("ashwani");
+let name ="ashwani"
+let age =19
+let isloggedIn= false
+
+
+// number=> 2 to power 53
+//digint
+// string=> ""
+// boolen =>  true/false
+// null => standlone value  //yai ek standerd value
+// undefined=>
+// symbole=> unique
+// object
+console.log(typeof "false");
+console.log(typeof age);
