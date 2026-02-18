18ea90632f1ccfb948a399d11cce44b9d503c9ef
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-02-18 04:27:36 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+//let score="13"
+//let score="13as"
+//let score=null
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ /*
+ "13"=> 13 number give exact vale.
+ "13as"=> NaN not a number
+ null=> 0
+ undefined=> NaN
+ true=> 1
+ false=> 0
+ */
+let isloggedIn=1
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+/*
+0, null, undefined, NaN, "" => false
+any other value like ashwani, ram ,2 etc=> true
+*/
+ let somenumber=43
+ let stringNumber=String(somenumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
