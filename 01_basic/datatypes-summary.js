67ecc03bc0b5786eaa6c704b710ab96cfba7724f
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-02-18 05:09:25 +0000

    datatypes-summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,9 @@
+// primitive data types
+// 7 types: string, number, boolean, null, undefined, symbol, bigint
+
+//refrence data types or non primitive data types
+// object, array, function
+// array is a type of object 
+// javascript is a dynamically typed language=> we dont have to specify data type of variable at the time of declaration
+// we can change the data type of variable at runtime
+
