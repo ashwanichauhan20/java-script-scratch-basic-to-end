b7778a00ead8cb861a58603fa282d6faecc4e706
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-02-19 16:26:26 +0000

    maths and num

---
+++ 01_basic/06nums_and_maths.js
@@ -0,0 +1,25 @@
+const score =200
+// we explain normaly in any number 
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 236.435
+console.log(otherNumber.toPrecision(3));
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//****************************************Maths ***************** */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.4));
+console.log(Math.ceil(4.3));
+console.log(Math.random()*10+1)
+const min =10
+const max = 23
+console.log(Math.floor(Math.random()*( max - min + 1 )) + min)
