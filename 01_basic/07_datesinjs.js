743b7f583c2e022a56306e7a5af009f1ad758407
Ashwani Chauhan <157831534+ashwanichauhan20@users.noreply.github.com>
2026-02-19 16:37:26 +0000

    date nad time

---
+++ 01_basic/07_datesinjs.js
@@ -0,0 +1,11 @@
+ // Date
+ let myDate = new Date()
+ console.log(myDate.toDateString())
+ console.log(myDate.toString())
+ console.log(myDate.toLocaleDateString())
+ console.log(typeof myDate)
+
+ let myCreatedDate =new Date(2023,0,23)
+ let myCratedDate = new Date(2023 ,0 ,23 ,5 ,3 )
+ 
+
